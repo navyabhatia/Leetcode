a988afe58dd5e52767048a67570d5e6030402040
navyabhatia <navyabhatia10@gmail.com>
2021-12-15 23:28:48 +0530

    Time:86 ms(9.25%), Space:9.7 MB(31.96%)

---
+++ 147-Insertion-Sort-List.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        for(auto i=head;  i!=NULL;  i=i->next){
+            for(auto j=head; j!=i ; j=j->next){
+                if(j->val > i->val)
+                    swap(j->val, i->val);
+            }
+        }
+        return head;
+    }
+};
